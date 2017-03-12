38705f39234d7e4cda71ec12a2f9c4bb1125e6fc
zhchi <zhchi_earth@163.com>
2017-03-12 22:20:49 +0800

    Signed-off-by: zhchi

--- 12/main.js
+++ 12/main.js
@@ -1,9 +1,12 @@
+window.Event = new Vue();
+
 Vue.component('coupon', {
     template: '<input placeholder="Enter your coupon code" @blur="onCouponApplied" >',
 
     methods: {
       onCouponApplied() {
-        this.$emit('applied');
+        // this.$emit('applied');
+        Event.$emit('applied');
 
       }
   }
@@ -12,9 +15,12 @@ Vue.component('coupon', {
 new Vue({
   el: '#root',
 
+  data: {
+    couponApplied: false
+  },
   methods: {
       onCouponApplied() {
-        alert('It was applied!');
+        this.couponApplied = true;
       }
   }
 });
